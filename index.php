ad58e4e3058f70d816f2716f060ed664bc4af32b
Fuk Pin Chen <Fuk Pin Chen>
2016-07-13 19:59:01 +0200

    Second initial

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello Worl
+<html>
+<header>
+<title>Hier kommt der Titel></title>
+</header>
+<body>
+<form action="#" method="get">
+  Add Book:<br>
+  <input type="text" name="buch">
+  <br>
+  Add Genre:<br>
+  <input type="text" name="genre">
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
